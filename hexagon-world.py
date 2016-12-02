bdbe374096b944a5370603ac6840301f2d33ac28
Willem <willemjjk@gmail.com>
2016-12-02 12:30:27 -0700

    Added css

--- hexagon-world.py
+++ hexagon-world.py
@@ -5,6 +5,11 @@ from bottle import route, run, template, static_file, request, redirect, respons
 def home():
     return template('home')
 
+
+@route('/css/<filename:path>')
+def css(filename):
+    return static_file(filename, root='css')
+
 # Run bottle internal test server when invoked directly ie: non-uxsgi mode
 if __name__ == '__main__':
     run(host="0.0.0.0", port=8080, debug=True)
